36ba467bf1b97f99c8f736a37b6e7536113e8c40
michaelnexus <michael@nexusbr.com>
2023-04-22 20:04:07 -0300

    Container Treino mock concluido

--- pages/components/treino/index.js
+++ pages/components/treino/index.js
@@ -1,16 +1,57 @@
-import { TreinoNome, Treino } from "./style";
+import { TreinoNome, TreinoContainer, ImageCardContainer } from "./style";
+import { RiArrowDownCircleFill, RiArrowUpCircleFill } from 'react-icons/ri'
+import { ImCheckboxUnchecked, ImCheckboxChecked } from 'react-icons/im'
+import { useState } from "react";
+
+
 
 export default function Treino() {
+
+  const [carga, setCarga] = useState(40);
+  const [status, setStatus] = useState(false)
+
+  const handleIncreaseCarga = function () {
+    setCarga(carga + 1)
+  }
+
+  const handleDecreaseCarga = function () {
+    setCarga(carga - 1)
+  }
+
+  const handleDone = function () {
+    setStatus(true)
+  }
+
+  const handleUndone = function () {
+    setStatus(false)
+  }
+
+
   return (
     <>
       <TreinoNome>
-        Supino Plano
+        <h3>Supino Plano</h3>
+        {status ? <ImCheckboxChecked size={20} onClick={handleUndone} />
+          : <ImCheckboxUnchecked size={20} onClick={handleDone} />}
+
+
 
       </TreinoNome>
 
-      <Treino>
+      <TreinoContainer>
+        <ImageCardContainer></ImageCardContainer>
 
-      </Treino>
+        <p>Sets: 12-10-8-6</p>
+        <p>Tipo: Progressão de carga</p>
+
+
+        <div id="carga">
+          Carga:
+          <RiArrowUpCircleFill onClick={handleIncreaseCarga} />
+          <p>{carga} Kg</p>
+          <RiArrowDownCircleFill onClick={handleDecreaseCarga} />
+        </div>
+      </TreinoContainer>
     </>
   )
 }
--- pages/components/treino/style.js
+++ pages/components/treino/style.js
@@ -1,13 +1,89 @@
 import styled from "styled-components";
 
-export const TreinoNome = styled.h3`
+export const TreinoNome = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
   font-weight: 700;
   font-size: 21px;
   line-height: 32px;
+  margin:  5% 7.5% 2.5%;
+
+  >svg{
+    width: 50px;
+  }
+  >h3{
+    width: 150px;
+  }
 `
 
-export const Treino = styled.div`
+export const ImageCardContainer = styled.div`
+  width: 100%;
+  height: 150px;
+  background: url("supinoPlano.webp");
+  background-size:cover ;
+  background-position: center;
+  margin: auto;
+  border-radius: 4px;
+
+  
+
+
+
+
+`
+
+export const TreinoContainer = styled.div`
   width: 355px;
-  height: 306px;
+  height: fit-content;
+  padding-bottom: 5%;
+ 
+
+ background: rgba(38, 87, 215, 0.25);
+  box-shadow: 2px 2px 4px rgba(38, 87, 215, 0.5);
+  border-radius: 4px;
+  margin: auto;
+  position: relative;
+
+  >p{
+  font-weight: 400;
+  font-size: 21px;
+  line-height: 32px;
+
+  background: transparent;
+  margin: 5% 0 0 5%;
+  }
+
+  >#carga{
+   display: flex;
+   width: 80%;
+   align-items: center;
+   font-weight: 400;
+    font-size: 21px;
+    line-height: 32px;
+    margin: 5% 0 0 5%;
+
+    background: transparent;
+ 
+    >p{
+      background: transparent;
+      text-align: center;
+    }
+
+   >svg{
+  
+    background: transparent;
+
+   }
+
+   
+   
+  }
+
+
+
+
+
 
 `
